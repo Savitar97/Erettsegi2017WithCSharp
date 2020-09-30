590851e9fe7bf18c19f0ff1bd6242a56f2b994ff
Attila Tóth <43468256+Savitar97@users.noreply.github.com>
2020-09-30 02:50:48 +0200

    Initial commit

---
+++ ErettsegiFeladat2017/Program.cs
@@ -0,0 +1,150 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Linq.Expressions;
+
+namespace ErettsegiFeladat2017
+{
+
+    class Program
+    {
+        public static float TuraTeljesHossza(List<Szakasz> szakaszok)
+        {
+            float szum = 0;
+            foreach (var element in szakaszok)
+            {
+                szum += element.Hossz;
+            }
+            return szum;
+        }
+
+        public static Szakasz Legrovidebb(List<Szakasz> szakaszok)
+        {
+            Szakasz legrovidebb = new Szakasz();
+            float shortest = szakaszok.Min(element => element.Hossz);
+            foreach(var element in szakaszok)
+            {
+                if (element.Hossz == shortest)
+                {
+                    legrovidebb = element;
+                }
+            }
+            return legrovidebb;
+        }
+
+        public static bool HianyosNev(Szakasz sz)
+        {
+            bool ret = false;
+            if (sz.SzakaszVege.Equals('i'))
+            {
+                if (!sz.Vegpont.Contains("pecsetelohely"))
+                {
+                    ret = true;
+                }
+            }
+            return ret;
+        }
+
+        public static string KiirFormat(Szakasz sz)
+        {
+            string temp = "";
+            if (HianyosNev(sz))
+            {
+                temp = sz.Vegpont + " pecsetelohely";
+            }
+            else
+            {
+                temp = sz.Vegpont;
+            }
+            string visszaad = String.Format($"{sz.Kiindulopont};{temp};{sz.Hossz};{sz.Emelkedes};{sz.Lejtes};{sz.SzakaszVege}");
+            return visszaad;
+        }
+
+        public static List<String> LegmagasabbVegpontesMagassaga(List<Szakasz> szakaszok,float kezdomagassag)
+        {
+            List<string> eredmeny = new List<string>();
+            float max= szakaszok.Max(element => element.Emelkedes-element.Lejtes);
+            Szakasz sz = szakaszok.Find(e => (e.Emelkedes - e.Lejtes) == max);
+            eredmeny.Add(sz.Vegpont);
+            float maxmagassag = 0;
+            foreach(var element in szakaszok)
+            {
+                maxmagassag += (element.Emelkedes - element.Lejtes);
+                if (element == sz)
+                {
+                    break;
+                }
+            }
+            eredmeny.Add((maxmagassag+kezdomagassag).ToString());
+            return eredmeny;
+        }
+
+        public static List<String> HianyosNevek(List<Szakasz> szakaszok)
+        {
+            List<String> pecsetelohely = new List<String>(); ;
+            foreach(var element in szakaszok)
+            {
+                if (HianyosNev(element))
+                {
+                    pecsetelohely.Add(element.Vegpont);
+                }
+            }
+            return pecsetelohely;
+        }
+
+
+        static void Main(string[] args)
+        {
+            List<Szakasz> adatok = new List<Szakasz>();
+            float tengerszint;
+            try
+            {
+                var kezdomagassag = Convert.ToInt32(File.ReadLines("kektura.csv").First());
+                tengerszint = kezdomagassag;
+                Console.WriteLine(kezdomagassag);
+                var beolvasott = File.ReadAllLines("kektura.csv");
+                foreach (var element in beolvasott.Skip(1))
+                {
+                    var sor = element.Split(";");
+     
+                    adatok.Add(new Szakasz(sor[0], sor[1], float.Parse(sor[2]), Int32.Parse(sor[3]), Int32.Parse(sor[4]), Char.Parse(sor[5])));
+
+                }
+            }
+            catch(Exception)
+            {
+                throw new FileNotFoundException();
+            }
+            
+            Console.WriteLine("3.feladat: Szakaszok száma:{0}",adatok.Capacity);
+            Console.WriteLine("4.feladat: A túra teljes hossza:{0:0.000} km",TuraTeljesHossza(adatok));
+            Console.WriteLine("5.feladat: A legrövidebb szakasz adatai:");
+            Console.WriteLine("Kezdete:{0}",Legrovidebb(adatok).Kiindulopont);
+            Console.WriteLine("Vége:{0}", Legrovidebb(adatok).Vegpont);
+            Console.WriteLine("Távolság:{0}", Legrovidebb(adatok).Hossz);
+            Console.WriteLine("7.feladat: Hiányos állománynevek:");
+            if (HianyosNevek(adatok).Count == 0)
+            {
+                Console.WriteLine("Nincs hiányos állománynév!");
+            }
+            else
+            {
+                foreach(var element in HianyosNevek(adatok))
+                {
+                    Console.WriteLine("{0}",element);
+                }
+            }
+            Console.WriteLine("8.feladat: A túra legmagasabban fekvő végpontja:");
+            Console.WriteLine("A végpont neve:{0}",LegmagasabbVegpontesMagassaga(adatok,tengerszint)[0]);
+            Console.WriteLine("A végpont tengerszintfeletti magassága:{0} m", LegmagasabbVegpontesMagassaga(adatok,tengerszint)[1]);
+            List<String> adatotkiir = new List<string>();
+            adatotkiir.Add(tengerszint.ToString());
+            foreach(var element in adatok)
+            {
+                adatotkiir.Add(KiirFormat(element));
+            }
+            File.WriteAllLines("kektura2.csv",adatotkiir);
+        }
+    }
+}
---
+++ ErettsegiFeladat2017/Szakasz.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ErettsegiFeladat2017
+{
+    public class Szakasz
+    {
+        public string Kiindulopont { get; set; }
+        public string Vegpont { get; set; }
+        public float Hossz { get; set; }
+        public int Emelkedes { get; set; }
+        public int Lejtes { get; set; }
+        public char SzakaszVege { get; set; }
+
+        public Szakasz()
+        {
+        }
+
+        public Szakasz(string kiindulopont, string vegpont, float hossz, int emelkedes, int lejtes, char szakaszVege)
+        {
+            Kiindulopont = kiindulopont;
+            Vegpont = vegpont;
+            Hossz = hossz;
+            Emelkedes = emelkedes;
+            Lejtes = lejtes;
+            SzakaszVege = szakaszVege;
+        }
+
+    }
+}
